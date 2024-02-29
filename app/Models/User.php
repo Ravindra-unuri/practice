5fc31108a3026c7362463ed719798b8f703d9691
Ravindra-unuri <ravindraunuri@gmail.com>
2024-02-29 17:51:00 +0530

    Formated date through cast

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,5 +47,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'created_at' => 'date:Y-m-d',
+        'updated_at' => 'date:Y-m-d',
     ];
 }
